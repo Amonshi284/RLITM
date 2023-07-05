e29bb55993154c7b3d47de0a87ea66095c49d53f
waeschephil <waeschephilipp@gmail.com>
2023-07-05 14:37:16 +0200

    more revising inputs

--- nine_mens_morris.py
+++ nine_mens_morris.py
@@ -1,4 +1,4 @@
-from ned import nmm_place
+# from ned import nmm_place
 
 board = [[0 for x in range(8)] for y in range(3)]
 
@@ -26,6 +26,15 @@ class Player:
                     if get_legal_moves(x, y) != "":
                         return True
         return False
+
+    def get_movable_tokens(self):
+        tokens = ""
+        for x in range(3):
+            for y in range(8):
+                if board[x][y] == self.token:
+                    if get_legal_moves(x, y) != "":
+                        tokens += x.__str__() + y.__str__() + " "
+        return tokens
 
     def player_input(self):
         valid = 0
@@ -59,7 +68,8 @@ class BotPlayer(Player):
             self.name = "\033[0;37;44mPlayer 2\033[0m"
 
     def move(self):
-        nmm_place(1)
+        # nmm_place(1)
+        print("place oder so")
 
 
 player1 = Player("X")
@@ -92,41 +102,28 @@ def print_board():
     string = string.replace("0", "╬")
     string = string.replace("X", "\033[0;37;41mX\033[0m")
     string = string.replace("O", "\033[0;37;44mO\033[0m")
-    string += "\n" + player1.name + " Current Tokens: " + player1.current_player_tokens().__str__() + " \t" + player2.name + " Current Tokens: " + player2.current_player_tokens().__str__()
+    string += "\n" + player1.name + " Current Tokens: " + player1.current_player_tokens().__str__() + " \t" + \
+              player2.name + " Current Tokens: " + player2.current_player_tokens().__str__()
     print(string)
-
-
-def _input_and_validation(player):
-    valid = 0
-    while valid == 0:
-        x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-        while x > 2 or x < 0:
-            print("incorrect input")
-            x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-        y = int(input("0 - 7 Clockwise\n"))
-        while y > 7 or y < 0:
-            print("incorrect input")
-            y = int(input("0 - 7 Clockwise\n"))
-
-        else:
-            board[x][y] = player.token
-            if token_check(x, y):
-                print("Mühle :D")
-                print_board()
-                remove_token(player)
-            valid = 1
 
 
 def check():
     for x in range(3):
         for y in range(0, 7, 2):
             if y > 4:
-                if board[x][y + 0] == board[x][y + 1] == board[x][0] and board[x][y + 0] != 0:
+                if board[x][y + 0] == board[x][y + 1] == board[x][0] and board[x][y + 0] != 0 and board[x][y] != "╬":
+                    print("Check 1")
+                    print(x.__str__() + y.__str__())
                     return True
-            elif board[x][y + 0] == board[x][y + 1] == board[x][y + 2] and board[x][y + 0] != 0:
+            elif board[x][y + 0] == board[x][y + 1] == board[x][y + 2] and board[x][y + 0] != 0 and board[x][y] != "╬":
+                print("Check 2")
+                print(x.__str__() + y.__str__())
                 return True
     for y in range(0, 7, 2):
-        if board[0][y + 1] == board[1][y + 1] == board[2][y + 1] and board[0][y + 0] != 0:
+        if board[0][y + 1] == board[1][y + 1] == board[2][y + 1] and board[0][y + 1] != 0 and board[x][y] != "╬":
+            print("Check 3")
+            print(x.__str__() + y.__str__())
+            print(board[0][y + 1].__str__() + board[1][y + 1].__str__() + board[2][y + 1].__str__())
             return True
     return False
 
@@ -193,50 +190,59 @@ def get_legal_moves(x, y):
     return legal_moves
 
 
-def move_token(player, x, y):
-    TODO Check legal moves before select!!
-    legal_moves = get_legal_moves(x, y)
-    print(legal_moves)
+def move_token(player):
+    tokens = player.get_movable_tokens()
+    print("Movable tokens: " + tokens)
+    print("Select token to move")
+    legal_moves = ""
     valid_input = False
     while not valid_input:
-        x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-        while x > 2 or x < 0:
-            print("incorrect input")
-            x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-        y = int(input("0 - 7 Clockwise\n"))
-        while y > 7 or y < 0:
-            print("incorrect input")
-            y = int(input("0 - 7 Clockwise\n"))
-        if legal_moves.__contains__(x.__str__() + y.__str__()):
-            board[x][y] = player.token
-            if token_check(x, y):
-                remove_token(player)
-            valid_input = True
+        a = player.player_input()
+        x = a[0]
+        y = a[1]
+        if not tokens.__contains__(x.__str__() + y.__str__()):
+            print("not a movable token select another!")
+            print("Movable tokens: " + tokens)
         else:
-            print("invalid position\nthe legal moves are: " + legal_moves)
+            if player.current_player_tokens() <= 3:
+                for i in range(3):
+                    for j in range(8):
+                        if board[i][j] == "":
+                            legal_moves += i.__str__() + j.__str__() + " "
+            else:
+                legal_moves = get_legal_moves(x, y)
+            valid_input = True
+    print("Select new position\nAvailable Position: " + legal_moves)
+    valid_input = False
+    while not valid_input:
+        a = player.player_input()
+        x = a[0]
+        y = a[1]
+        if not tokens.__contains__(x.__str__() + y.__str__()):
+            print("not a valid position select another!")
+            print("legal moves: " + legal_moves)
 
 
-def _remove_token(player):
+def remove_token(player):
     print_board()
     print("Select token to remove")
     valid_input = False
     while not valid_input:
-        x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-        while x > 2 or x < 0:
-            print("incorrect input")
-            x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-        y = int(input("0 - 7 Clockwise\n"))
-        while y > 7 or y < 0:
-            print("incorrect input")
-            y = int(input("0 - 7 Clockwise\n"))
+        a = player.player_input()
+        x = a[0]
+        y = a[1]
         if board[x][y] == player.token:
             print("Cant remove your own token")
-        if board[x][y] == 0:
+            player.reward(-1)
+        elif board[x][y] == 0:
             print("position empty")
-        if token_check(x, y) and free_token_exits():
+            player.reward(-1)
+        elif token_check(x, y) and free_token_exits():
             print("Please select a free token")
+            player.reward(-1)
         elif board[x][y] != 0 and board[x][y] != player.token:
             board[x][y] = 0
+            player.reward(1)
             valid_input = True
 
 
@@ -252,6 +258,7 @@ def check_token_count():
 
 def phase_one():
     stones_placed = 0
+    current_player = Player
     while stones_placed <= 17:
         print(stones_placed)
         valid = False
@@ -272,6 +279,9 @@ def phase_one():
                 valid = True
             print_board()
         stones_placed += 1
+        if check():
+            print("Mühle")
+            remove_token(current_player)
 
 
 def phase_two():
@@ -282,32 +292,11 @@ def phase_two():
     turns_without_mill = 0
     while turns_without_mill < 20:
         if turn % 2 == 0:
-            player = player1
+            current_player = player1
         else:
-            player = player2
+            current_player = player2
         turn += 1
-        valid_input = False
-        if not player.has_legal_moves():
-            print(player.name + " has no legal moves and loses")
-            return
-        print("Select token to move")
-        while not valid_input:
-            x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-            while x > 2 or x < 0:
-                print("incorrect input")
-                x = int(input(player.name + " Outside = 0, Center = 1, Inside = 2\n"))
-            y = int(input("0 - 7 Clockwise\n"))
-            while y > 7 or y < 0:
-                print("incorrect input")
-                y = int(input("0 - 7 Clockwise\n"))
-            if board[x][y] != player.token:
-                print("you can only move your own token")
-            elif board[x][y] == 0:
-                print("position empty")
-            else:
-                valid_input = True
-        board[x][y] = 0
-        move_token(player, x, y)
+        move_token(current_player)
         print_board()
         if check():
             turns_without_mill = 0
